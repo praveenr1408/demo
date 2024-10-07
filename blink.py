c7cedf00452ec2108ea05b5cb790b098b9e592e0
praveenr1408 <praveengabap@gmail.com>
2024-10-07 21:26:54 +0530

    demo checking

---
+++ blink.py
@@ -0,0 +1,13 @@
+import machine
+import time
+
+led = machine.Pin(2, machine.Pin.OUT)  # Assuming GPIO2 is the LED pin
+
+def blink_led():
+    while True:
+        led.value(1)  # Turn on LED
+        time.sleep(1)
+        led.value(0)  # Turn off LED
+        time.sleep(1)
+
+blink_led()
