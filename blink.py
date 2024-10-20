2bab80116f90b7060c874fe72452a7f9409c2038
Praveen R <144775786+praveenr1408@users.noreply.github.com>
2024-10-21 00:16:41 +0530

    Update blink.py

--- blink.py
+++ blink.py
@@ -6,8 +6,8 @@ led = machine.Pin(2, machine.Pin.OUT)  # Assuming GPIO2 is the LED pin
 def blink_led():
     while True:
         led.value(1)  # Turn on LED
-        time.sleep(1)
+        time.sleep(2)
         led.value(0)  # Turn off LED
-        time.sleep(1)
+        time.sleep(2)
 
 blink_led()
